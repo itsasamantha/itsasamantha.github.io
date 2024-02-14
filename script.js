d46a8d8a8239f2952cea0c4bc5f266b6e1b500f3
itsasamantha <spowell@brooklynsteamcenter.org>
2024-02-14 14:51:58 -0500

    Added photos and testing parallax effect

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(function () {
-    'use strict';
-    var view = $(window).height();
-    $('.part')
-        .height(view)
-        .scrollie({
-            scrolloffset: -50,
-            scrollingInView: function (elem) {
-                var bgColor = elem.data('background');
-                $('body').css('background-color', bgColor);
-            }
-        });
-});
+// $(function () {
+//     'use strict';
+//     var view = $(window).height();
+//     $('.part')
+//         .height(view)
+//         .scrollie({
+//             scrolloffset: -50,
+//             scrollingInView: function (elem) {
+//                 var bgColor = elem.data('background');
+//                 $('body').css('background-color', bgColor);
+//             }
+//         });
+// });
